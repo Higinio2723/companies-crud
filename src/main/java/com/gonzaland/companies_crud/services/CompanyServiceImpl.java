6d88391dbcb540846f286cf9d15d9586ef839294
hgonzalezsr <gonzacah23@hotmail.com>
2026-01-20 22:21:45 -0600

    Se realiza cambio para el servicio

--- src/main/java/com/gonzaland/companies_crud/services/CompanyServiceImpl.java
+++ src/main/java/com/gonzaland/companies_crud/services/CompanyServiceImpl.java
@@ -1,13 +1,25 @@
 package com.gonzaland.companies_crud.services;
 
+import com.gonzaland.companies_crud.entities.Category;
 import com.gonzaland.companies_crud.entities.Company;
 import com.gonzaland.companies_crud.repositories.CompanyRepository;
+import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
+import java.io.ObjectStreamClass;
+import java.util.Objects;
 import java.util.Optional;
 
 
+@Service
+@Transactional
+@Slf4j
+@AllArgsConstructor
 public class CompanyServiceImpl implements CompanyService {
 
     @Autowired
@@ -15,18 +27,23 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public Company create(Company company) {
-        return companyRepository.save(company);
+        company.getWebSites().forEach(webSite -> {
+                    if (Objects.isNull(webSite.getCompany())) {
+                        webSite.setCategory(Category.NONE);
+                    }
+                }
+        );
+
+   return companyRepository.save(company);
+
     }
 
 
     @Override
     public Company readByName(String name) {
-        Optional<Company> companyOptional = companyRepository.findByName(name);
-        if (companyOptional.isEmpty()) {
-            return null;
-        }
 
-        return companyOptional.get();
+        return companyRepository.findByName(name)
+                .orElseThrow(()-> new RuntimeException("Company not found"));
     }
 
 

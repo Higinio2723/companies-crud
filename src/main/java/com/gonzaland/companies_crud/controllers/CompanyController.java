11a82459a0e398e6030c018f850d6791f42f10cd
Higinio Gonzalez Castro <gonzacah27@hotmail.com>
2026-01-22 10:18:53 -0600

    Se realiza cambios para conectarse al config-server

--- src/main/java/com/gonzaland/companies_crud/controllers/CompanyController.java
+++ src/main/java/com/gonzaland/companies_crud/controllers/CompanyController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(path = "/api/company")
+@RequestMapping(path = "company")
 @Slf4j
 @Tag(name = "Companies resource")
 public class CompanyController {
